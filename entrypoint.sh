2b7652df64554af91159334e2c26688c44edca1c
Kjuly <dev@kjuly.com>
2024-05-12 22:49:02 +0800

    entrypoint.sh: Add test index.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,12 @@ mappings=$2
 
 printf 'Default Language: %s\nMappings:\n%s\n' "$default_lang" "$mappings"
 
+if [ ! -d "build" ]; then
+  mkdir "build"
+  touch "build/index.html"
+fi
+echo "Test Index Page" > "build/index.html"
+
 if [ -z "$mappings" ]; then
   sphinx-build -M html "src/$default_lang" "build" -D language="$default_lang"
   return
