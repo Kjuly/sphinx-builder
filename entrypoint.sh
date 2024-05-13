73d69f35f57bca49a2f92e3667c940987d751fd4
Kjuly <dev@kjuly.com>
2024-05-13 09:55:02 +0800

    Update entrypoint.sh: Use /src dir directly for one language case

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ mappings=$2
 printf 'Default Language: %s\nMappings:\n%s\n' "$default_lang" "$mappings"
 
 if [ -z "$mappings" ]; then
-  sphinx-build -M html "src/$default_lang" "build" -D language="$default_lang"
+  sphinx-build -M html "src" "build" -D language="$default_lang"
   return
 fi
 
